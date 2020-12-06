a60d2ced36f0b9834520a9c1e84497cf3e5c7502
Anuj Verma <68846777+anujvdev2020@users.noreply.github.com>
2020-12-06 16:36:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const details={
+	email:'anujvdev2020@gmail.com',
+	pwd:"anujv@1998"
+}
+
+btn.onclick=()=>{
+	if(email.value===details.email && pwd.value===details.pwd){
+	alert("Welcome")
+}
+else{
+	alert("Wrong Credientials")
+}
+}
+
+
+
+	
