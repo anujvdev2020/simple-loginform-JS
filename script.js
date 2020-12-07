5d83ebdec2d4ff67375253b8a23ec84aa98de090
Anuj Verma <68846777+anujvdev2020@users.noreply.github.com>
2020-12-07 23:42:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,16 +2,53 @@ const details={
 	email:'anujvdev2020@gmail.com',
 	pwd:"anujv@1998"
 }
+const nd={}
 
 btn.onclick=()=>{
-	if(email.value===details.email && pwd.value===details.pwd){
-	alert("Welcome")
-}
-else{
-	alert("Wrong Credientials")
-}
-}
+	let option=btn.innerHTML;
+	if(option==="Sign Up"){
 
+
+
+
+		nd.details=email.value;
+		nd.pwd= pwd.value;
+		nd.ph=phn.value;
+		alert("Details Saved")
+	}
+
+	else{
+		if(email.value===details.email && pwd.value===details.pwd){
+	alert("Welcome")
+	}
+	else{
+	alert("Wrong Credientials")
+	}
+
+	}
 
 
 	
+
+
+}
+
+
+
+
+signup.onclick=()=>{
+	h1.innerHTML="Sign Up To Continue";
+	let phone=document.createElement("input");
+	phone.placeholder="Enter Phone Number";
+	phone.id="phn"
+	document.getElementById('new').appendChild(phone); 
+
+btn.innerHTML="Sign Up"
+
+
+
+
+}
+
+
+
